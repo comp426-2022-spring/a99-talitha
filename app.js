ad108991c0fd1db033341dee313f9f50d1340b4d
ianwash <ianwash@live.unc.edu>
2022-03-25 00:37:38 -0400

    added organization for views and routes

--- app.js
+++ app.js
@@ -2,10 +2,19 @@
 var express = require("express");
 var path = require("path");
 
+//var routes = require("./routes");
+
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
 
-app.listen(app.get("port"), function(){
+//tells express where the views are 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", require("./routes/web"));
+app.use("/api", require("./routes/api"));
+
+app.listen(app.get("port"),function(){
     console.log("Sever started on port " + app.get("port"));
-})
+});
