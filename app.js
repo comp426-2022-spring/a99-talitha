b74966692538c159ffed0f05b2e6eaeb8ce78553
Kadhir Patchamuthu <kadhirp@ad.unc.edu>
2022-04-06 18:36:28 -0400

    Added the first part of the login/databse side (still has errors)

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 // main application file 
 var express = require("express");
 var path = require("path");
+var bodyPaser = require("body-parser");
+var mongoose = require("mongoose");
+var cookieParser = require("cookie-parser");
+var passport = require("passport");
+var session = require("express-session");
+var flash = require("connect-flash");
+var params = require("./params/params");
 
-
+var setUpPassport = require("./setuppassport");
 var app = express();
 
+mongoose.connect(params.DATABASECONNECTION, {useUnifiedTopology:true, useNewUrlParser:true, useCreateIndex:true});
+setUpPassport();
 app.set("port", process.env.PORT || 3000);
 
 //tells express where the views are 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(sesion({
+    secret:"thisisabunchofgarbagestring",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 
 app.use("/", require("./routes/web"));
 app.use("/api", require("./routes/api"));
--- routes/web/home.js
+++ routes/web/home.js
@@ -1,5 +1,8 @@
 // allows navigation between subpages
 var express = require("express");
+var passport = require("passport");
+
+var User = require("../../models/user");
 
 var router = express.Router();
 
@@ -22,6 +25,31 @@ router.get("/login", function(req,res) {
 router.get("/signup", function(req,res) {
     res.render("home/signup");
 });
+
+router.post("/signup", function(req, res, next) {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({email:email}, function(err, user){
+        if(err){return next(err);}
+        if(user){
+            req.flash("error", "There's already an account with that email");
+            return res.redirect("/signup");
+        }
+        var newUser = new User({
+            username:username,
+            password:password,
+            email:email
+        });
+        newUser.save(next);
+    });
+
+}, passport.authenticate("login", {
+    successRedirect:"/",
+    failureRedirect:"/signup",
+    failureFlash:true
+}));
 
 router.get("/account", function(req,res) {
     res.render("home/account");
