86080f92714881751a7d26743f63612e4f7d4409
vennila-t <69809909+vennila-t@users.noreply.github.com>
2022-04-28 15:20:04 -0400

    removed api keys

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import "firebase/compat/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDG7rI2uVabmklOBYVuHE5nY7CpCqrKS7g",
-  authDomain: "comp426-b1832.firebaseapp.com",
-  projectId: "comp426-b1832",
-  storageBucket: "comp426-b1832.appspot.com",
-  messagingSenderId: "412395717971",
-  appId: "1:412395717971:web:d0e530901578c4e618cda6",
-  measurementId: "G-7M4B9QYCGV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
